02fc05e6c3639d2e66a6c0a4444d006108c1b658
Eric W. Erdmann <eric.erdmann79@gmail.com>
2022-09-02 11:54:53 -0400

    working on unit test 1

--- tests/settings.py
+++ tests/settings.py
@@ -25,41 +25,37 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'material.frontend',
+    'viewflow.frontend', 
+    'viewflow',   
     'material',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'viewflow.frontend',
-    'viewflow',
-    'tests',
-    'demo.customnode',
-    'demo.helloworld',
-    # 'demo.shipment',
+    'material.frontend',
+    'material.admin',
+
+#    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    'datahub.pipeline'
 )
 
-MIDDLEWARE = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
 
 ROOT_URLCONF = 'tests.urls'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config() or {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db{}{}.sqlite3'.format(*django.VERSION[:2])),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR,"db.sqlite3"),
     }
 }
 
