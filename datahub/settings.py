b2be3612785bc26ad0307a710662a35c1d9c3af0
Eric W. Erdmann <eric.erdmann79@gmail.com>
2022-09-02 13:13:32 -0400

    working on unit test 9 ./manage.py test tests settings correction all asertions passed but not in pipline

--- datahub/settings.py
+++ datahub/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'material.frontend',
     'material.admin',
  
-#    "django.contrib.admin",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
