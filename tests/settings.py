0ebdc59f68a6b880ee1f097ac13d85bd2c6938a3
Eric W. Erdmann <eric.erdmann79@gmail.com>
2022-09-05 15:50:09 -0400

    test container 0

--- tests/settings.py
+++ tests/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = (
     'material.frontend',
     'material.admin',
 
-#    "django.contrib.admin",
+    #"django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
